074b26faa364ec555aa0a30795ae05cf9fc9148c
jakobpfeifer135 <138628595+jakobpfeifer135@users.noreply.github.com>
2023-09-25 21:43:09 -0500

    added comments to javascript to better define what everything does

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var generateBtn = document.querySelector("#generate");
-
+// individual arrays of characters that share a certain type
 var specialCharacters = [
   "@", "%", "+", "\\", "/", "'", "!", "#", "$", "^", "?", ":", ",", ")", "(", "}", "{", "]", "[", "~", "-", "_", "."
 ];
@@ -10,7 +10,7 @@ var upperCasedCharacters = [
 var lowerCasedCharacters = [
   "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
 ];
-
+// prompt that asks user the length of their desired password and makes it be between 8 and 128 characters
 function passwordOptions() {
   var userInput = prompt("How many characters would you like in your password?");
 
@@ -25,12 +25,12 @@ function passwordOptions() {
     alert("Please enter a valid number between 8 and 128.");
     return passwordOptions();
   }
-
+//sends the user a ok, cancel box that asks what all they want inside their password
   var confirmSpecialCharacters = confirm("Do you want to include special characters?");
   var confirmNumberCharacters = confirm("Do you want to include numbers?");
   var confirmUppercaseCharacters = confirm("Do you want to include uppercase characters?");
   var confirmLowercaseCharacters = confirm("Do you want to include lowercase characters?");
-
+// an if statement saying they must choose at least one option
   if (
     !confirmSpecialCharacters &&
     !confirmNumberCharacters &&
@@ -49,7 +49,7 @@ function passwordOptions() {
     confirmUppercaseCharacters
   ];
 }
-
+//does the randomization of characters * the length of the password the user selects
 function getRandomPassword(arr) {
   var randomIndex = Math.floor(Math.random() * arr.length);
   return arr[randomIndex];
@@ -61,6 +61,7 @@ function generate() {
   if (!options) {
     return null;
   }
+  //makes the options match the order in which the user selects yes or no to the password criteria
   var length = options[0];
   var hasLowercaseCharacters = options[1];
   var hasNumberCharacters = options[2];
@@ -68,7 +69,7 @@ function generate() {
   var hasUppercaseCharacters = options[4];
 
   var possiblePassword = [];
-
+// a concat function that will string one character after another once user selects criteria
   if (hasSpecialCharacters) {
     possiblePassword = possiblePassword.concat(specialCharacters);
   }
@@ -88,12 +89,12 @@ function generate() {
     var randomCharacter = getRandomPassword(possiblePassword);
     passwordResults.push(randomCharacter);
   }
-
+//combine the arrays with the characters together and return the final result
   var finalPassword = passwordResults.join("");
   console.log(finalPassword);
   return finalPassword;
 }
-
+// display the final password to the user
 function writePassword() {
   var password = generate();
   if (password !== null) {
@@ -101,7 +102,7 @@ function writePassword() {
     passwordText.value = password;
   }
 }
-
+//a call of a function when the generate button is clicked
 generateBtn.addEventListener("click", writePassword);
 
 // GIVEN I need a new, secure password
